ece654354aea52774f37371fbecd6914e0d57029
dzikoysk <dzikoysk@dzikoysk.net>
2021-09-02 14:33:55 +0200

    Improve assertion messages in JUnit module and release 1.0.7

--- expressible-junit/src/main/java/panda/std/ResultAssertions.java
+++ expressible-junit/src/main/java/panda/std/ResultAssertions.java
@@ -1,25 +1,31 @@
 package panda.std;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static panda.std.Result.error;
 import static panda.std.Result.ok;
 
 public final class ResultAssertions {
 
     public static void assertOk(Result<?, ?> result) {
-        assertTrue(result.isOk());
+        if (result.isErr()) {
+            fail("Expected success, but result errored with: <" + result.getError() + ">");
+        }
     }
 
     public static <VALUE> void assertOk(VALUE value, Result<VALUE, ?> result) {
+        assertOk(result);
         assertEquals(ok(value), result);
     }
 
     public static void assertError(Result<?, ?> result) {
-        assertTrue(result.isErr());
+        if (result.isOk()) {
+            fail("Expected failure, but result succeeded with: <" + result.get() + ">");
+        }
     }
 
     public static <ERROR> void assertError(ERROR error, Result<?, ERROR> result) {
+        assertError(result);
         assertEquals(error(error), result);
     }
 
