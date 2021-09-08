4567747839199d302bee9db7dcde615b04d5df38
dzikoysk <dzikoysk@dzikoysk.net>
2021-09-08 16:24:38 +0200

    GH-9 Use rx prefix

--- expressible-kt-coroutines/src/main/kotlin/panda/std/coroutines/RxResultExtensions.kt
+++ expressible-kt-coroutines/src/main/kotlin/panda/std/coroutines/RxResultExtensions.kt
@@ -3,14 +3,14 @@ package panda.std.coroutines
 import panda.std.Result
 import panda.std.Result.ok
 
-suspend fun <VALUE, ERROR, MAPPED> Result<VALUE, ERROR>.map(mapper: suspend (VALUE) -> MAPPED): Result<MAPPED, ERROR> =
+suspend fun <VALUE, ERROR, MAPPED> Result<VALUE, ERROR>.rxMap(mapper: suspend (VALUE) -> MAPPED): Result<MAPPED, ERROR> =
     if (isOk) ok(mapper(get())) else projectToError()
 
-suspend fun <VALUE, ERROR, MAPPED> Result<VALUE, ERROR>.flatMap(mapper: suspend (VALUE) -> Result<MAPPED, ERROR>): Result<MAPPED, ERROR> =
+suspend fun <VALUE, ERROR, MAPPED> Result<VALUE, ERROR>.rxFlatMap(mapper: suspend (VALUE) -> Result<MAPPED, ERROR>): Result<MAPPED, ERROR> =
     if (isOk) mapper(get()) else projectToError()
 
-suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.peek(consumer: suspend (VALUE) -> Unit): Result<VALUE, ERROR> =
+suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.rxPeek(consumer: suspend (VALUE) -> Unit): Result<VALUE, ERROR> =
     if (isOk) also { consumer(get()) } else this
 
-suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.onError(consumer: suspend (ERROR) -> Unit): Result<VALUE, ERROR> =
+suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.rxOnError(consumer: suspend (ERROR) -> Unit): Result<VALUE, ERROR> =
     if (isErr) also { consumer(error) } else this
