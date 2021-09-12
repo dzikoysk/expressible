22a29955b29a849d9f3c0d66458419669520ccbb
dzikoysk <dzikoysk@dzikoysk.net>
2021-09-12 21:45:13 +0200

    Add firstSuccessOr & firstOrErrors and release 1.0.15

--- expressible-kt-coroutines/src/main/kotlin/panda/std/coroutines/RxResultExtensions.kt
+++ expressible-kt-coroutines/src/main/kotlin/panda/std/coroutines/RxResultExtensions.kt
@@ -1,5 +1,8 @@
 package panda.std.coroutines
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import panda.std.Result
 import panda.std.Result.ok
 
@@ -14,3 +17,16 @@ suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.rxPeek(consumer: suspend (VALUE)
 
 suspend fun <VALUE, ERROR> Result<VALUE, ERROR>.rxOnError(consumer: suspend (ERROR) -> Unit): Result<VALUE, ERROR> =
     if (isErr) also { consumer(error) } else this
+
+suspend fun <VALUE, ERROR, MAPPED_ERROR> Flow<Result<out VALUE, ERROR>>.firstSuccessOr(elseValue: suspend () -> Result<out VALUE, MAPPED_ERROR>): Result<out VALUE, MAPPED_ERROR> =
+    this.firstOrNull { it.isOk }
+        ?.projectToValue()
+        ?: elseValue()
+
+suspend fun <VALUE, ERROR> Flow<Result<out VALUE, ERROR>>.firstOrErrors(): Result<out VALUE, Collection<ERROR>> {
+    val collection: MutableCollection<ERROR> = ArrayList()
+
+    return this
+        .map { result -> result.onError { collection.add(it) } }
+        .firstSuccessOr { Result.error(collection) }
+}
